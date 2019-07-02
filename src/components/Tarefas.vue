8267ba937ea3236df56a37404753619823e3ae72
Nata <natajonas.programador@eucatur.com.br>
2019-07-02 09:34:24 -0400

    Tarefa Lista

--- src/components/Tarefas.vue
+++ src/components/Tarefas.vue
@@ -1,16 +1,29 @@
 <template>
-<q-page class="flex flex-center">
+
+<q-page class="flex">
+
     <q-list bordered padding>
-        <q-item>
+
+        <q-item v-for="(fruit, index) in fruits" :key="index" >
+
             <q-item-section>
-                <q-item-label overline>OVERLINE</q-item-label>
                 <q-item-label>Single line item</q-item-label>
                 <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-                <q-icon name="star" color="yellow"/>
             </q-item-section>
-        </q-item>
+
+            <q-item-section side center>
+                <q-icon name="close" color="red"/>
+            </q-item-section>
+
+      </q-item>
+      
+      <q-separator bordered inset />
+
+
     </q-list>
+
 </q-page>
+
 </template>
 
 <style>
@@ -19,8 +32,13 @@
 <script>
 export default {
   name: 'Tarefas.vue',
+    data() {
+        return {
+            fruits: ['apple', 'banana', 'orange']
+        };
+    },
   props: {
-      tarefas: Array
+      tarefas: Array,
   }
 }
 </script>
